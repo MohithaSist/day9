a8cc9c21301fb2e2e0a42a428381b3b190cb83d1
MohithaSist <101502266+MohithaSist@users.noreply.github.com>
2022-12-24 23:21:04 -0800

    Update disarium.py

--- disarium.py
+++ disarium.py
@@ -18,37 +18,6 @@ else:
     print("False")
             
         
-'''a=int(input())
-c=str(a)
-l1=[]
-st=''
-count=0
-k=len(c)
-for i in str(a):
-    
-for j in range(1,k+1):
-        
-        if j==1:
-            m=pow(int(i),j) 
-            st=''+str(m)
-            print(st)'''
-
-
-'''l1=[1,2,3,4]
-l2=[1,2,3,4]
-l3=[]
-for i in range(0,len(l1)):
-    m=l1[i]**l2[i]
-    print(m)'''
-
-            
-
-
-
-
-
-
-
 
 
 
